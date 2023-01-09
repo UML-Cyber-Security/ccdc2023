b00609d2ab7da6f89115929339875c4c3a1774d9
Manoj Sarma Yeddanapudi <manoj.sarma7@gmail.com>
2023-01-09 01:22:41 -0500

    Added scripts to add and remove Wazuh agents

---
+++ Scripts/SOC/Baremetal_Install/playbooks/add_agent_debian.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -so wazuh-agent-4.3.10.deb https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.3.10-1_amd64.deb && sudo WAZUH_MANAGER='192.168.0.121' WAZUH_AGENT_GROUP='default' dpkg -i ./wazuh-agent-4.3.10.deb
+
+# Restarts and enables the agent
+ systemctl daemon-reload
+ systemctl enable wazuh-agent
+ systemctl start wazuh-agent
