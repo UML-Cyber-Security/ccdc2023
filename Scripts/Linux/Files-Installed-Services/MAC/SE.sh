6d58058a948fc72dfcb93b6319d8a99d7adb5119
DaintyJet <Matther_harper30@aol.com>
2023-01-13 23:53:50 -0500

    Matt-Final

--- Scripts/Linux/Files-Installed-Services/MAC/SE.sh
+++ Scripts/Linux/Files-Installed-Services/MAC/SE.sh
@@ -6,7 +6,7 @@ if [ -f "/etc/debian_version" ]; then
     selinux-activate
 elif [ -f "/etc/redhat-release" ]; then
     yum install sudo yum install policycoreutils policycoreutils-python setools setools-console setroubleshoot
-    
+    selinux-activate
 elif [ -f "/etc/arch-release" ]; then
     echo "Arch, Will this come up -- probably should do fedora"
 fi
