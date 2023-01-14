4fd554757a9f0c658e3791b9a13f360670c04eb8
aallman1 <102977324+aallman1@users.noreply.github.com>
2023-01-13 21:52:34 -0500

    Create disable_mfa.py

---
+++ Scripts/Linux/Password/disable_mfa.py
@@ -0,0 +1,27 @@
+from os import system
+
+def disableMFA(usernames):
+    for user in usernames: 
+        system(f"sudo usermod -a -G disable_mfa {user}")
+
+        
+def addGroup(group_name):
+    system(f"sudo groupadd {group_name}")
+        
+if __name__ == "__main__":
+    group_name = "disable_mfa"         
+    disable_group = []
+     
+    with open("disable_mfa.txt", "r") as mLog:
+        usrs = mLog.readlines()
+        for usr in usrs:
+            disable_group.append(usr.rstrip())   
+            
+    if len(disable_group) != 0:
+        addGroup(group_name)        
+        disableMFA(disable_group)
+    
+        with open("/etc/pam.d/sshd", "a") as pLog:
+            pLog.write(f"auth [success=done default=ignore] pam_succeed_if.so user ingroup {group_name}\n")
+            
+        
