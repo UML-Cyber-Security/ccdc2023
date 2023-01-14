d2b03ed2fa7e5afdef75d25d22d1c37e6d494fd7
aallman1 <102977324+aallman1@users.noreply.github.com>
2023-01-13 21:04:16 -0500

    Update chg_usr_pws.py

--- Scripts/Linux/Password/chg_usr_pws.py
+++ Scripts/Linux/Password/chg_usr_pws.py
@@ -4,7 +4,7 @@ from os import system
 from random import choices
 import string
 
-usr_accts, ref = [], ["bash", "sh", "zsh"]
+usr_accts, ref, uid_values = [], ["bash", "sh", "zsh"], []
 
 root_pw = '1qazxsW@1' #nothing to see here
 
@@ -13,19 +13,40 @@ def run():
     with open("/etc/passwd") as uLog:
         usrs = uLog.readlines()
         for usr in usrs:
+            usr_values = usr.rstrip().split(":")
             if any(r in usr for r in ref):
-                usr_accts.append(usr.rstrip().split(":")[0])
-                
-    #Set new passwords 
-    with open("usr_pw_prs.txt", "w") as pLog:           
-        for usr in usr_accts:
-            if usr == 'root':
-                pr = (usr, root_pw)
-            else:
-                pr = (usr, ''.join((choices(string.ascii_lowercase, k=9))))
-            system(f'echo "{pr[0]}:{pr[1]}" | sudo chpasswd')
-            pLog.write(str(pr) + "\n")
+                usr_accts.append((usr_values[0], usr_values[2]))
+                uid_values.append(eval(usr_values[2]))
 
+    max_uid = max(sorted(uid_values, reverse=True))
+    
+    #Set new passwords
+    with open("/temp_store.txt", "w") as pLog:
+        pLog.write("**********User Account Modifications**********\n")
+        for usr in usr_accts:
+            if usr[0] == 'root':
+                pr = (usr[0], root_pw, usr[1])
+            else:
+                resp = ''
+                while (resp != "yes" and resp != "no"):
+                   resp = input(f"Would you like to change the password for user (if yes, type 'yes', if no type 'no'): {usr[0]}? ")
+
+                if resp == "yes":
+                    pr = (usr[0], ''.join((choices(string.ascii_lowercase, k=16))), usr[1])
+                    system(f'echo "{pr[0]}:{pr[1]}" | sudo chpasswd')
+                    pLog.write(str(pr) + "\n")
+            if usr[1] == '0':
+                resp = ''
+                while (resp != 'yes' and resp != 'no'):
+                    resp = input(f"UID 0 found for user {usr[0]}, would you like to change: ")
+                if resp == 'yes':
+                   print(f"Setting {usr[0]} UID to {max_uid + 1}")
+                   system(f'usermod -u {max_uid + 1} {usr[0]}')
+                   max_uid += 1
+                   pLog.write(f"{usr[0]} UID changed to {max_uid}\n")
+                else:
+                    pLog.write(f"***{usr[0]} has UID value 0***\n")
+        pLog.write("**********************************************\n")
 
 if __name__ == "__main__":
     run()
